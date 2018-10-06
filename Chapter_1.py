d2da282c7b6413bca7708572153a5843a4dd724b
Stuart McRobbie <simcrobbie@gmail.com>
2018-10-05 22:03:16 -0500

    Chapter 1 Challenge, a plan for learning

---
+++ Chapter_1.py
@@ -0,0 +1,14 @@
+#Challenge
+
+#1.) Create a daily checklist that includes practicing programming
+
+
+
+
+#List:
+
+#Task 1.) Finish self taught programmer book
+#Task 2.) Complete CS 50 course, videos and assignments
+#Task 3.) Start building little python projects, bigger with practice
+#Task 4.) ?????
+#Task 5.) Be featured in Forbes 30 under 30
