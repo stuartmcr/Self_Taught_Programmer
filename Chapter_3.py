8ab5c8b3dfaffac31f740cd9eeae7a84048a4a77
Stuart McRobbie <simcrobbie@gmail.com>
2018-10-08 22:28:49 -0500

    first bit of chapter 3

---
+++ Chapter_3.py
@@ -0,0 +1,29 @@
+#Challenge
+
+#1.) Print three different strings
+print("string one")
+print("another string")
+print("the last string")
+
+#2.) Write a program that prints a message if a variable is less than 10, and 
+#    different message if the variable is greater than or equal to 10
+x = int(input("pick a number between 1 and 20: "))
+
+if x >= 10:
+    print("x is greater than or equal to 10")
+else:
+    print("x is less than 10")
+
+#3.) Write a program than prints a message if a variable is less than or equal 
+#    to 10, another message if the variable is greater than 10 but less than or
+#    equal to 25, and another message if the variable is greater than 25
+
+
+#4.) Create a program that divides two variables and prints the remainer
+
+
+#5.) Create a program that takes two variables, divides them, and prints the quotient
+
+
+#6.) Write a program with a variable 'age' assigned to an integer that prints
+#    different strings depending on what integer 'age' is
