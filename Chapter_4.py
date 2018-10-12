86fdc6d86a0ec93adb8ee67137a5768d36b93c0e
Stuart McRobbie <simcrobbie@gmail.com>
2018-10-12 16:25:31 -0500

    prob1

---
+++ Chapter_4.py
@@ -0,0 +1,28 @@
+#Challenge
+
+#1.) Write a function that takes a number as an input and returns that number squared
+def f(x):
+    return x**2
+
+value = int(input("give me a number between 3 and 9: "))
+y = f(value)
+print(y)
+
+#2.) Create a function that accepts a string as a parameter and prints it
+
+
+#3.) Write a function that takes three required parameters and two optional parameters
+
+
+#4.) Write a program with two functions. The first function should take an integer
+#    as a parameter and return the result of the integer divided by 2. The second
+#    function should take an integer as a parameter and return the result of the 
+#    integer multiplied by 4. Call the first function, save the result as a 
+#    variable, and pass it as a parameter to the second function
+
+
+#5.) Write a function that converts a string to a float and returns the result. Use
+#    exception handling to catch the exception that could occur
+
+
+#6.) Add a docstring to all of the functions you wrote in challenges 1-5
