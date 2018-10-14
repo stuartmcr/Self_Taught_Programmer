b9c664194ba9ef6d0fabe89cd9d39cb18983f719
Stuart McRobbie <simcrobbie@gmail.com>
2018-10-14 16:51:17 -0500

    chap5 template set up

---
+++ Chapter_5.py
@@ -0,0 +1,24 @@
+#Challenges
+
+#1.) Create a list of your favorite musicians
+
+
+#2.) Create a list of tuples, with each tuple containing the longitude
+#    and latitude of somewhere you've lived or visited
+
+
+#3.) Create a dictionary that contains different attributes about
+#    you: height, favorite color, favorite author, etc.
+
+
+#4.) Write a program that lets the user ask your height, favorite color, or
+#    favorite author, and returns the result from the dictionary you created 
+#    in the previous challenge
+
+
+#5.) Create a dictionary mapping your favorite musicians to a list of your
+#    favorite songs by them
+
+
+#6.) Lists, tuples, and dictionaries are just a few of the containers built
+#    into Python. Research Python sets. When would you use a set?
