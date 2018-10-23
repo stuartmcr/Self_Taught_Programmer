06d8be40999a3caec5119de46c53cdefe328cfce
Stuart McRobbie <simcrobbie@gmail.com>
2018-10-22 22:45:59 -0500

    through probs 3 and 4

--- Chapter_5.py
+++ Chapter_5.py
@@ -13,12 +13,18 @@ print(houston)
 
 #3.) Create a dictionary that contains different attributes about
 #    you: height, favorite color, favorite author, etc.
-
+stu_stu = {"height":74, "color":"blue", "author":"dan brown", "artist":"kygo"}
+print(stu_stu)
 
 #4.) Write a program that lets the user ask your height, favorite color, or
 #    favorite author, and returns the result from the dictionary you created 
 #    in the previous challenge
-
+a = input("What would you like to know - height, color, author, or artist? ")
+if a in stu_stu:
+    descrip = stu_stu[a]
+    print(descrip)
+else:
+    print("Not Found")
 
 #5.) Create a dictionary mapping your favorite musicians to a list of your
 #    favorite songs by them
