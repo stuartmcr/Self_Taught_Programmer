d61bce5a83e7df1ec32c91063646f3a55d34b009
Stuart McRobbie <simcrobbie@gmail.com>
2018-10-30 21:07:34 -0500

    prob 1+2

---
+++ Chapter_7.py
@@ -0,0 +1,23 @@
+#Challenges
+
+#1.) print each item in the following list: ["the walking dead", "entourage",
+#    "the sopranos", "vampire diaries"]
+shows = ["the walking dead", "entourage", "the sopranos", "vampire diaries"]
+for show in shows:
+    print(show)
+
+#2.) print all the numbers from 25 to 50
+for i in range(25, 51):
+    print(i)
+
+#3.) print each item in the list from the first challenge and their indexes
+
+
+#4.) write a program with an infiite loop(q to quit) and a list of numbers.
+#    each time through the loop ask the user to guess a number on the list
+#    and tell them whether or not they guesses correctly
+
+
+#5.) Multiply all the numbers in the list [8, 19, 148, 4] with all the
+#    numbers in the list [9, 1, 33, 83], and append each result to a third list
+
